12493d73b429ce205ce8556ae6756d9a8afea552
xiaoxiao <969872569@qq.coom>
2023-02-12 17:02:10 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import CommonFunction from '@/utils/index.js'
 import App from './App.vue'
+import '@/assets/reset.css'//引入基础样式表
+import '@/assets/iconfont/iconfont.css'
+import router from './router/index'//
+import VueParticles from 'vue-particles'
+import axios from 'axios'
+//引入markdown编辑 vue主题
+import VueMarkdownEditor from "@/plugins/markdown.js"
 
-createApp(App).mount('#app')
+//引入Markdown html 解析模块
+import VMdPreviewHtml from "@/plugins/markdownHtml.js"
+
+//预览模板
+import VMdPreview from "@/plugins/markdownPreview.js"
+
+const app = createApp(App)
+const pinia = createPinia()
+app.use(VMdPreviewHtml);
+app.use(VMdPreview);
+app.use(VueMarkdownEditor);
+ app.use(router)
+ app.use(VueParticles)
+ app.use(CommonFunction)
+ app.use(pinia)
+//  app.config.warnHandler = (msg, instance, trace) => {}
+ app.mount('#app')
+ app.provide('$axios',axios)
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave:false,
+  devServer: {
+    // 设置代理
+    proxy: {
+      '/api': {
+        // target: 'http://localhost:8888/', // 开发环境下使用
+        target: 'http://localhost:8888', // 生产环境下需要根据实际情况修改
+        changeOrigin: true, //改变源
+        // pathRewrite: {
+        //   '^/api': '/' //路径重写
+        // },//配置上诉代码 相当于 将/api 替换成 /
+        // pathRequiresRewrite: {
+        //   '^/api': ''
+        // }
+      }
+    }
+  },
 })
